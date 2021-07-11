fb79438313de598b4fec87f826bbdcf90bc0518b
Sourabh Gour <saurabhgour619@gmail.com>
2021-07-11 12:46:17 +0530

    login code refactored.

--- src/main/java/com/app/controller/UserLoginController.java
+++ src/main/java/com/app/controller/UserLoginController.java
@@ -1,5 +1,7 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,10 @@ import com.app.service.UserService;
 @RestController
 public class UserLoginController {
 
+	@Autowired
 	private UserService userService;
 
+	@PostMapping("/login/user")
 	public String loginUser(@RequestBody UserLoginRequest userLoginRequest) {
 		boolean isValidUser = userService.checkLogin(userLoginRequest);
 		return isValidUser ? "Login Successful" : "Invalid credentials";
